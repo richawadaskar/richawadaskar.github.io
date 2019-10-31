a3c9957601185272be47d8f5695d5f73cdf3ce01
Richa Wadaskar <richa.wadaskar@gmail.com>
2019-10-31 08:38:48 -0700

    uploading frontend

---
+++ precache-manifest.3f8e37b8fbfb806b13e2990090ffda80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b0bf328db020b1470c1533de4dcb91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5606d6742d162061361b",
+    "url": "/static/css/main.b0ea553a.chunk.css"
+  },
+  {
+    "revision": "30250e2c2c7bd6bc2853",
+    "url": "/static/js/2.0f52d97f.chunk.js"
+  },
+  {
+    "revision": "5606d6742d162061361b",
+    "url": "/static/js/main.1f2e677a.chunk.js"
+  },
+  {
+    "revision": "cd2485c73a32870978d1",
+    "url": "/static/js/runtime-main.5f18b2d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
